b28570bd9d6ac222a141096480d85d5d342039fa
KaraB <kara.b@windowslive.com>
2017-12-02 14:04:56 +0100

    polish

--- src/MyScreen.java
+++ src/MyScreen.java
@@ -7,10 +7,12 @@ import java.awt.event.KeyEvent;
 public class MyScreen extends Screen{
 
     private final Player player;
+    private final KeyboardListener keyboardListener;
 
     public MyScreen(Game game, Player player) {
         super(game);
         this.player = player;
+        this.keyboardListener = game.getKeyboardListener();
     }
 
     @Override
@@ -26,28 +28,30 @@ public class MyScreen extends Screen{
 
     @Override
     public void onDraw(Graphics2D g2d) {
+        drawPlayer(g2d);
+    }
+
+    private void drawPlayer(Graphics2D g2d) {
         g2d.drawImage(player.getImage(),player.getX(), player.getY(), null);
     }
 
 
     private void checkInputs() {
-        if(getGame().getKeyboardListener().isKeyPressed(KeyEvent.VK_W)){
+        if(keyboardListener.isKeyPressed(KeyEvent.VK_W)){
             player.setY(player.getY() - 1);
         }
 
-        if(getGame().getKeyboardListener().isKeyPressed(KeyEvent.VK_A)){
+        if(keyboardListener.isKeyPressed(KeyEvent.VK_A)){
             player.setX(player.getX() - 1);
         }
 
-        if(getGame().getKeyboardListener().isKeyPressed(KeyEvent.VK_S)){
+        if(keyboardListener.isKeyPressed(KeyEvent.VK_S)){
             player.setY(player.getY() + 1);
         }
 
-        if(getGame().getKeyboardListener().isKeyPressed(KeyEvent.VK_D)){
+        if(keyboardListener.isKeyPressed(KeyEvent.VK_D)){
             player.setX(player.getX() + 1);
         }
-
     }
-
 
 }
